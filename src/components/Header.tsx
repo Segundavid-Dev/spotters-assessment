858e27b485c8793925fa0f39cf231ab106f49f94
Segundavid-Dev <segdavid03@gmail.com>
2025-07-23 20:52:23 +0100

    fixing header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,12 @@ import { Earth } from "lucide-react";
 import { Plane } from "lucide-react";
 import { Hotel } from "lucide-react";
 import { House } from "lucide-react";
+import { Menu } from "lucide-react";
 
 // react router navlink
 import { NavLink } from "react-router";
+
+import { useState } from "react";
 
 const navLinks: NavLinks = [
   {
@@ -43,10 +46,23 @@ const navLinks: NavLinks = [
 ];
 
 export default function Header() {
+  const [showNav, setShowNav] = useState<boolean>(false);
+
+  const showNavMobile = () => {
+    setShowNav((prev) => !prev);
+  };
+
   return (
-    <header className="sticky top-0 left-0 shadow-2xs w-full z-50">
-      <nav className="bg-white h-18 flex items-center max-w-7xl mx-auto px-8">
-        <ul className="flex gap-4">
+    <header className="sticky top-0 left-0 shadow-2xs w-full z-50 max-sm:absolute">
+      <nav className="bg-white h-16 flex items-center max-w-7xl mx-auto px-8 max-sm:h-screen max-sm:w-[80%] max-sm:mx-0 max-sm:block">
+        <span
+          onClick={showNavMobile}
+          className="mr-5 hover:bg-[var(--secondary)] p-2 rounded-full duration-200 cursor-pointer"
+        >
+          <Menu />
+        </span>
+
+        <ul className="flex gap-4 max-sm:flex-col">
           {navLinks.map((link, index) => (
             <NavLink to={link.linkTo} key={index}>
               <li key={index}>
